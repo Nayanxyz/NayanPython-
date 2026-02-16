40e2042ef760f7cea5fcbc85984e5e7ce676f902
NayanPython <choudharyji527@gmail.com>
2026-02-16 15:29:47 +0530

    Convertor Feets to Meters Project3

---
+++ Convertor_Feet_Meter.py
@@ -0,0 +1,6 @@
+def convert(feets,inches):
+  feet = float(feets)
+  inches = float(inches)
+  meters = feet*0.3048 + inches*0.0254
+  return meters
+
---
+++ Convertor_Feet_Meter_Project3.py
@@ -0,0 +1,34 @@
+from Convertor_Feet_Meter import convert
+
+import FreeSimpleGUI as g
+
+label = g.Text("Convertor_Feet_Meter")
+
+enter_feet = g.Text("Enter Feet")
+feet_input = g.InputText(tooltip="Enter Feet", key="feet")
+
+
+enter_inch = g.Text("Enter Inches")
+inch_input = g.InputText(tooltip="Enter Inches",key="inches")
+
+convert_button = g.Button("Convert")
+
+meter_text = g.Text(key="output", text_color="Black")
+
+window = g.Window("Convertor",layout=[[label], [enter_feet, feet_input], [enter_inch, inch_input],[convert_button, meter_text]],
+                    font=['Helvetica',12])
+
+while True:
+    event , values = window.read()
+    if event == g.WIN_CLOSED:
+        break
+    if event == "Convert":
+
+        feet = values["feet"]
+        inches = values["inches"]
+
+        result = convert(feet,inches)
+        window["output"].update(value=f"{result} Meters")
+
+
+window.close()
