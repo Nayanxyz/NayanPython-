d1697996bbb105b4c2c38f25810d6be69cf7c23a
NayanPython <choudharyji527@gmail.com>
2026-02-17 11:00:38 +0530

    Added Replace/Remove/Clock, change theme

--- GUI.py
+++ GUI.py
@@ -2,21 +2,32 @@ import FunctionComb
 
 import FreeSimpleGUI as FSG                                                      # FSG is short
 
+import time
+
+FSG.theme("GreenMono")
+
+clock = FSG.Text("",key='clock')
 label = FSG.Text("Type in a TO-DO")                                              #Text is type in FSG
 input_box = FSG.InputText(tooltip="Enter todo", key="todo")                      #tooltip is a tool to hover over box and InputText is also a Type
-add_button =FSG.Button("Add")                                                    #Button is also a type for buttons
+add_button =FSG.Button("Add", size=7)                                                    #Button is also a type for buttons
 list_box = FSG.Listbox(values=FunctionComb.todos_f(), key="todos",
                        enable_events=True , size=[45,10])
 replace_button = FSG.Button("Replace")
-window = FSG.Window('My To-Do App', layout=[[label],[input_box, add_button],[list_box, replace_button]],
-                    font=['Helvetica',20])
+remove_button = FSG.Button("Remove")
+exit_button = FSG.Button("Exit", size=7)
+window = FSG.Window('My To-Do App',  layout=[[clock],[label],[input_box, add_button],
+                            [list_box, replace_button, remove_button],
+                            [exit_button]],font=['Helvetica',20])
                                                                                  #Window is a type of FSG / layout is a tool
                                                                                  #[] list shows rows ..if add_button were to be in separate
                                                                                  # list then it would be on another row
 while True:
-    event, values = window.read()                                                #read method not to read but to display on window methods like title()
-    print(1,event)
-    print(2,values)
+    event, values = window.read(timeout=200)                                     #read method not to read but to display on window methods like title()
+    if event != FSG.WIN_CLOSED:                                                  #clock is running even after i closed the window , so used if
+        window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    #print(1,event) #printing values in terminal helps us understand values and keys , very important thing i guess
+    #print(2,values)
+    #print(3, values['todos'])
     match event:
         case "Add":
             todos = FunctionComb.todos_f()
@@ -25,17 +36,34 @@ while True:
             todos.append(new_todo)                                           #update method to update current list in real time in todos file
             FunctionComb.todos_w(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value="")                             #update input_box with empty " "
         case "Replace":
-            todo_to_replace = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_replace = values['todos'][0]
+                new_todo = values['todo']                                   #remember key to put values in[]
 
-            todos = FunctionComb.todos_f()
-            index = todos.index(todo_to_replace)
-            todos[index]= new_todo
-            FunctionComb.todos_w(todos)
-            window['todos'].update(values=todos)
+                todos = FunctionComb.todos_f()
+                variable = todos.index(todo_to_replace)                     #storing every event in separate variables
+                todos[variable]= new_todo
+                FunctionComb.todos_w(todos)
+                window['todos'].update(values=todos)                        #[todos] is key and values=todos is todos list
+            except IndexError:
+                FSG.popup("Please select a To-Do", font=['Helvetica',20])
+        case "Remove":
+            try:
+                todo_to_remove = values['todos'][0]
+                todos = FunctionComb.todos_f()
+                todos.remove(todo_to_remove)                               # remove method
+                FunctionComb.todos_w(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                FSG.popup("Please select to Remove",font=['Helvetica',20])
+
+        case "Exit":
+            break
         case 'todos':
-            window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0])      #when user clicked a line in list , it shows in input box ,
 
         case FSG.WIN_CLOSED:
             break
